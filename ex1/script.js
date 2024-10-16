ee5affa9e6c760123b7cabdf5e874a2e1d740494
Liedson <sr.liedsonloco@gmail.com>
2024-10-16 11:48:48 -0300

    Update

--- ex1/script.js
+++ ex1/script.js
@@ -3,7 +3,7 @@ let hora = agora.getHours()
 let text1 = document.querySelector("#conteudo1 > p")
 let body = document.getElementById("body")
 
-hora = 6
+hora = 21
 
 text1.innerHTML = `O horario atual é: <strong>${hora} horas</strong>`
 
